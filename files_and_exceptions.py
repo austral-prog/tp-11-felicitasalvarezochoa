9ff30a12ce58466d6b74161f229c83045fd2ec59
felicitasalvarezochoa <falvarezochoa@mail.austral.edu.ar>
2025-06-22 18:57:18 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -15,3 +15,11 @@ def read_file_to_dict(filename):
                 ventas_dict[producto].append(valor)
 
     return ventas_dict
+
+
+def process_dict(data):
+    for producto in sorted(data.keys()):
+        lista_ventas = data[producto]
+        total = sum(lista_ventas)
+        promedio = total / len(lista_ventas)
+        print(f"{producto}: ventas totales ${total:.2f}, promedio ${promedio:.2f}")
